483847d83fa9069172e6c1daf002873a624a3022
unknown <reachtolaghavi@gmail.com>
2024-01-05 20:29:28 -0700

    my testt commit

--- src/main/java/com/ls/la/DockerJenkinGit/MyController.java
+++ src/main/java/com/ls/la/DockerJenkinGit/MyController.java
@@ -12,7 +12,7 @@ public class MyController {
 	public String getMessage()
 	{
 		
-		return "Hello Laghs. This is cool and u shud know that";
+		return "Hello Devi maa. This is cool and u shud know that";
 	}
 
 }
